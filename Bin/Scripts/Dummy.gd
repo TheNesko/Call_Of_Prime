63b0854aa89258001f130289197f5d74539f4b40
TheNesko <neskoplayer@gmail.com>
2022-01-31 04:58:04 +0100

    DPS timer bug fix

--- Bin/Scripts/Dummy.gd
+++ Bin/Scripts/Dummy.gd
@@ -3,23 +3,29 @@ extends StaticBody2D
 var DamagePerSecond = 0
 var DamageDone = 0.00
 var DPS_time = 0.00
+var DamagedAmmount = 0
 
 func _Take_Damage(damage):
 	DamageDone += damage
+	DamagedAmmount += 1
 	$DPSTimer.start()
 	if DPS_time == 0:
 		yield(get_tree().create_timer(0.05),"timeout")
-	DamagePerSecond = DamageDone/DPS_time
+	if DamagedAmmount == 1:
+		DamagePerSecond = DamageDone
+	else:
+		DamagePerSecond = DamageDone/DPS_time
 
-func _process(delta):
+func _physics_process(delta):
 	if !$DPSTimer.is_stopped():
-		$DPSlabel.visible = true
 		$DPSlabel.text = "Damage per second: " + str(round(DamagePerSecond))\
 					+ "   Damage dealt: " + str(DamageDone)
+		$DPSlabel.visible = true
 	else:
 		$DPSlabel.visible = false
 	if $DPSTimer.is_stopped():
 		DPS_time = 0
+		DamagedAmmount = 0
 		return
 	DPS_time += delta
 
